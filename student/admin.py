64ae0a302792d789893afb422ee13a53a3fbcfad
Kiran <kgawas9@gmail.com>
2022-05-25 22:44:34 +0530

    added models in student app

--- student/admin.py
+++ student/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Category, Course
+from .models import Category, Course, CourseEnrollment, CourseItem, Student
 
 # Register your models here.
 @admin.register(Category)
@@ -13,4 +13,23 @@ class CategoryAdmin(admin.ModelAdmin):
 class CourseAdmin(admin.ModelAdmin):
     list_display = [
         'id', 'title', 'description', 'fees', 'last_updated', 'category'
+    ]
+
+@admin.register(Student)
+class StudAdmin(admin.ModelAdmin):
+    list_display = [
+        'first_name', 'last_name', 'birth_date', 'membership'
+    ]
+
+
+@admin.register(CourseEnrollment)
+class CourseEnrollmentAdmin(admin.ModelAdmin):
+    list_display = [
+        'id', 'payment_status', 'enrolled_at'
+    ]
+
+@admin.register(CourseItem)
+class CourseItemAdmin(admin.ModelAdmin):
+    list_display = [
+        'id', 'quantity', 'unit_price'
     ]
--- student/models.py
+++ student/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib import admin
 
 from django.core.validators import MinValueValidator
+from django.conf import settings
 
 # Create your models here.
 class Category(models.Model):
@@ -27,4 +29,56 @@ class Course(models.Model):
     def __str__(self):
         return self.title
     
+class Student(models.Model):
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+
+    STUD_MEMBERSHIP = [
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Silver'),
+        (MEMBERSHIP_GOLD, 'Gold'),
+    ]
     
+    mobile_no = models.CharField(max_length=15, unique=True)
+    birth_date = models.DateField()
+    membership = models.CharField(max_length=1, choices=STUD_MEMBERSHIP, default=MEMBERSHIP_BRONZE)
+
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user.first_name}'
+
+    @admin.display(ordering='user__first_name')
+    def first_name(self):
+        return self.user.first_name
+
+    @admin.display(ordering='user__last_name')
+    def last_name(self):
+        return self.user.last_name
+
+class CourseEnrollment(models.Model):
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAILED = 'F'
+    PAYMENT_STATUS_CHOICES = [
+        (PAYMENT_STATUS_PENDING, 'Pending'),
+        (PAYMENT_STATUS_COMPLETE, 'Complete'),
+        (PAYMENT_STATUS_FAILED, 'Failed')
+    ]
+    
+    enrolled_at = models.DateTimeField(auto_now=True)
+    payment_status = models.CharField(
+        max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
+    student = models.ForeignKey(Student, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f'{self.student}'
+
+class CourseItem(models.Model):
+    enrollment = models.ForeignKey(CourseEnrollment, on_delete=models.PROTECT, related_name='items')
+    course = models.ForeignKey(
+        Course, on_delete=models.PROTECT, related_name='courseitems')
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
